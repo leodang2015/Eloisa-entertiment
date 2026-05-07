bc2cb58e71da170a67e8599e4e493a09f166aa5d
leodang2015 <leodang2015@gmail.com>
2026-05-06 20:51:35 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const cards = document.querySelectorAll(".card");
+
+// Botones de flechas
+const btnIzquierda = document.querySelector(".columna-1");
+const btnDerecha = document.querySelector(".columna-2");
+
+// Índice actual
+let indice = 0;
+
+// Función para mostrar tarjeta
+function mostrarCard(index) {
+
+    // Oculta todas
+    cards.forEach(card => {
+        card.classList.remove("activo");
+    });
+
+    // Muestra la actual
+    cards[index].classList.add("activo");
+}
+
+// Flecha derecha
+btnDerecha.addEventListener("click", () => {
+
+    indice++;
+
+    // Si llega al final vuelve al inicio
+    if (indice >= cards.length) {
+        indice = 0;
+    }
+
+    mostrarCard(indice);
+});
+
+// Flecha izquierda
+btnIzquierda.addEventListener("click", () => {
+
+    indice--;
+
+    // Si pasa del inicio va al final
+    if (indice < 0) {
+        indice = cards.length - 1;
+    }
+
+    mostrarCard(indice);
+});
